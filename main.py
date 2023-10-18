a294f885b999323e8f84d09fe53f6d677a27431b
zarazebra <sarah.iber.90@gmail.com>
2023-10-18 20:53:11 +0200

    Paddle Class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from turtle import Screen
+from paddle import Paddle
 
 window = Screen()
 window.title("Breakout Game")
 window.bgcolor("black")
+window.setup(800, 600)
+
+paddle = Paddle()
 
 window.exitonclick()
 window.mainloop()
