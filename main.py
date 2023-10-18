15989617bdd631d87e031a6f01e99458815d5678
zarazebra <sarah.iber.90@gmail.com>
2023-10-18 21:09:50 +0200

    Paddle movement

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ window.title("Breakout Game")
 window.bgcolor("black")
 window.setup(800, 600)
 
+
 paddle = Paddle()
+window.onkey(paddle.move_right, "Right")
+window.onkey(paddle.move_left, "Left")
+window.listen()
 
 window.exitonclick()
 window.mainloop()
--- paddle.py
+++ paddle.py
@@ -5,9 +5,14 @@ class Paddle(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
-        self.setheading(90)
+        self.setheading(0)
         self.goto(0, -270)
         self.shape("square")
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shapesize(stretch_wid=1, stretch_len=5)
         self.color("white")
 
+    def move_right(self):
+        self.forward(10)
+
+    def move_left(self):
+        self.back(10)
