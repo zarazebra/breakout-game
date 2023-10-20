4bed68b903bbd376088ffe47308a44df38d77a76
zarazebra <sarah.iber.90@gmail.com>
2023-10-20 17:17:22 +0200

    Ball Paddle collision

--- ball.py
+++ ball.py
@@ -5,6 +5,19 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
-        self.goto(0, -135)
+        self.goto(0, -100)
         self.shape("circle")
         self.color("white")
+        self.speed(1)
+        self.x_dir = 10
+        self.y_dir = -10
+
+    def moving(self):
+        self.goto(self.xcor() + self.x_dir, self.ycor() + self.y_dir)
+
+    def detect_paddle(self, paddle):
+        if self.ycor() <= -250:
+            if paddle.xcor() - 50 <= self.xcor() <= paddle.xcor() + 50:
+                self.x_dir *= -1
+                self.y_dir *= -1
+
--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 
+game_is_on = True
+
 window = Screen()
 window.title("Breakout Game")
 window.bgcolor("black")
 window.setup(800, 600)
-
-ball = Ball()
 paddle = Paddle()
+ball = Ball()
+
 window.onkey(paddle.move_right, "Right")
 window.onkey(paddle.move_left, "Left")
 window.listen()
 
+while game_is_on:
+    ball.moving()
+    ball.detect_paddle(paddle)
+
+
 window.exitonclick()
-window.mainloop()
+
